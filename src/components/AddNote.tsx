8cbe3ed15c62a4a563abb62140e0832d23a6ba3d
Oded2 <111157775+Oded2@users.noreply.github.com>
2025-06-19 02:27:07 +0300

    Enhance AddNote component with state management and add Button for note submission

--- src/components/AddNote.tsx
+++ src/components/AddNote.tsx
@@ -1,4 +1,5 @@
-import { useId } from "react";
+import { useEffect, useId, useState } from "react";
+import Button from "./Button";
 
 type Props = {
   label?: string;
@@ -7,19 +8,26 @@ type Props = {
 
 export default function AddNote({ label, placeholder }: Props) {
   const id = useId();
+  const [note, setNote] = useState("");
 
   return (
     <div className="max-w-xl flex flex-col gap-2">
       <label className="me-auto italic font-medium" htmlFor={id}>
         {label}
       </label>
-      <textarea
-        id={id}
-        dir="auto"
-        placeholder={placeholder}
-        className="bg-slate-800 resize-none outline-none focus:ring-2 focus:ring-slate-50/30 transition-shadow p-4 rounded-2xl"
-        rows={10}
-      ></textarea>
+      <div className="bg-slate-800 flex flex-col rounded-2xl focus-within:ring focus-within:ring-slate-50/30 transition-shadow pt-4 px-4 pb-2">
+        <textarea
+          onInput={(e) => setNote(e.currentTarget.value)}
+          id={id}
+          dir="auto"
+          placeholder={placeholder}
+          className="resize-none outline-none"
+          rows={10}
+        ></textarea>
+        <div className="my-1 flex justify-end">
+          <Button label="Add" isPrimary></Button>
+        </div>
+      </div>
     </div>
   );
 }
