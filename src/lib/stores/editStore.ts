d714f5de6fc048c7f2f15dc348e84e17835ab4dd
Oded2 <111157775+Oded2@users.noreply.github.com>
2025-06-20 16:49:25 +0300

    Remove commented-out code

--- src/lib/stores/editStore.ts
+++ src/lib/stores/editStore.ts
@@ -11,30 +11,3 @@ export const useEditStore = create<EditNoteStore>((set) => ({
   update: (note) => set({ note }),
   reset: () => set({ note: undefined }),
 }));
-
-// export const useNoteStore = create<NoteStore>((set) => ({
-//   notes: [],
-//   addNote: (title, content) =>
-//     set((state) => {
-//       const notes = [
-//         ...state.notes,
-//         { id: crypto.randomUUID(), title, content },
-//       ];
-//       updateLocalStorage(notes);
-//       return { notes };
-//     }),
-//   editNote: (id, title, content) =>
-//     set((state) => {
-//       const notes = state.notes.map((note) =>
-//         note.id === id ? { id, title, content } : note,
-//       );
-//       updateLocalStorage(notes);
-//       return { notes };
-//     }),
-//   removeNote: (id) =>
-//     set((state) => {
-//       const notes = state.notes.filter((note) => note.id !== id);
-//       updateLocalStorage(notes);
-//       return { notes };
-//     }),
-// }));
