df9f8c9ef22fdb329243920c7b11edf89813348d
Oded2 <111157775+Oded2@users.noreply.github.com>
2025-06-21 22:49:40 +0300

    Improved UI

--- src/components/NoteViewer.tsx
+++ src/components/NoteViewer.tsx
@@ -2,7 +2,7 @@ import { useEditStore } from "@/lib/stores/editStore";
 import { Note } from "@/lib/types";
 import { deleteDoc } from "firebase/firestore";
 import { AnimatePresence, motion } from "framer-motion";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useMemo } from "react";
 import Collapse from "./Collapse";
 import { handleError } from "@/lib/helpers";
 import CopyButton from "./CopyButton";
@@ -34,81 +34,81 @@ export default function NoteViewer({
     deleteDoc(note.ref).catch(handleError);
   };
 
-  return (
-    <div className="flex flex-col rounded-lg border border-slate-950/20">
-      <AnimatePresence mode="popLayout">
-        {notes.length > 0 ? (
-          notes.map((note) => {
-            const {
-              ref: { id },
-            } = note;
-            const isOpen = !closedNotes.includes(id);
-            return (
-              <motion.div
-                key={id}
-                layout
-                animate={{ opacity: 1, scale: 1 }}
-                exit={{ opacity: 0, scale: 0.9, y: 20 }}
-                transition={{ duration: 0.3 }}
-                className="group flex flex-col gap-2 border-slate-950/20 px-5 py-5 not-last:border-b"
-              >
-                <div className="flex items-center justify-between">
-                  <div className="flex flex-col">
-                    <h2 className="text-xl font-bold">
-                      {note.title || "Untitled"}
-                    </h2>
-                    <span className="text-sm text-slate-950/80">
-                      {note.createdAt.toDate().toLocaleString(undefined, {
-                        minute: "numeric",
-                        hour: "numeric",
-                        day: "numeric",
-                        month: "long",
-                        year: "numeric",
-                      })}
-                    </span>
-                  </div>
-                  <div className="flex opacity-0 transition-opacity duration-75 group-hover:opacity-100 pointer-coarse:opacity-100">
-                    <CopyButton text={note.content ?? ""} />
-                  </div>
+  return notes.length > 0 ? (
+    notes.map((note) => {
+      const {
+        ref: { id },
+      } = note;
+      const isOpen = !closedNotes.includes(id);
+      return (
+        <div
+          className="flex flex-col rounded-lg border border-slate-950/20"
+          key={id}
+        >
+          <AnimatePresence mode="popLayout">
+            <motion.div
+              layout
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.9, y: 20 }}
+              transition={{ duration: 0.3 }}
+              className="group flex flex-col gap-2 border-slate-950/20 px-5 py-5 not-last:border-b"
+            >
+              <div className="flex items-center justify-between">
+                <div className="flex flex-col">
+                  <h2 className="text-xl font-bold">
+                    {note.title || "Untitled"}
+                  </h2>
+                  <span className="text-sm text-slate-950/80">
+                    {note.createdAt.toDate().toLocaleString(undefined, {
+                      minute: "numeric",
+                      hour: "numeric",
+                      day: "numeric",
+                      month: "long",
+                      year: "numeric",
+                    })}
+                  </span>
                 </div>
-                <Collapse open={isOpen}>
-                  <p className="whitespace-pre-wrap">
-                    {note.content || "No content"}
-                  </p>
-                </Collapse>
-                <div className="me-auto mt-auto flex items-baseline gap-2 *:cursor-pointer *:hover:underline">
-                  {isOpen ? (
-                    <button
-                      onClick={() => setClosedNotes((state) => [...state, id])}
-                    >
-                      Close
-                    </button>
-                  ) : (
-                    <button
-                      onClick={() =>
-                        setClosedNotes((state) =>
-                          state.filter((noteId) => noteId !== id),
-                        )
-                      }
-                    >
-                      Open
-                    </button>
-                  )}
-                  <button onClick={() => setEditNote(note)}>Edit</button>
+                <div className="flex opacity-0 transition-opacity duration-75 group-hover:opacity-100 pointer-coarse:opacity-100">
+                  <CopyButton text={note.content ?? ""} />
+                </div>
+              </div>
+              <Collapse open={isOpen}>
+                <p className="whitespace-pre-wrap">
+                  {note.content || "No content"}
+                </p>
+              </Collapse>
+              <div className="me-auto mt-auto flex items-baseline gap-2 *:cursor-pointer *:hover:underline">
+                {isOpen ? (
                   <button
-                    onClick={() => handleDelete(note)}
-                    className="text-red-400"
+                    onClick={() => setClosedNotes((state) => [...state, id])}
                   >
-                    Delete
+                    Close
                   </button>
-                </div>
-              </motion.div>
-            );
-          })
-        ) : (
-          <div>No notes</div>
-        )}
-      </AnimatePresence>
-    </div>
+                ) : (
+                  <button
+                    onClick={() =>
+                      setClosedNotes((state) =>
+                        state.filter((noteId) => noteId !== id),
+                      )
+                    }
+                  >
+                    Open
+                  </button>
+                )}
+                <button onClick={() => setEditNote(note)}>Edit</button>
+                <button
+                  onClick={() => handleDelete(note)}
+                  className="text-red-400"
+                >
+                  Delete
+                </button>
+              </div>
+            </motion.div>{" "}
+          </AnimatePresence>
+        </div>
+      );
+    })
+  ) : (
+    <div>No notes</div>
   );
 }
