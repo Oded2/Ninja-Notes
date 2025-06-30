02f1c7928fcd036f3ffe6b874baf2dce5273727d
Oded2 <111157775+Oded2@users.noreply.github.com>
2025-07-01 02:01:33 +0300

    Update InputContainer to align items vertically for better layout consistency

--- src/components/InputContainer.tsx
+++ src/components/InputContainer.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 export default function InputContainer({ children }: Props) {
   return (
-    <div className="flex flex-wrap items-baseline gap-2 rounded-2xl bg-gray-100 px-4 py-3 ring ring-slate-950/20 transition-all focus-within:ring-2 focus-within:ring-slate-950">
+    <div className="flex flex-wrap items-center gap-2 rounded-2xl bg-gray-100 px-4 py-3 ring ring-slate-950/20 transition-all focus-within:ring-2 focus-within:ring-slate-950">
       {children}
     </div>
   );
