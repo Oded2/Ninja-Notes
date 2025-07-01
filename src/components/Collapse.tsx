284315a2a8628e42fe5a15ce5f367104f9867c59
Oded2 <111157775+Oded2@users.noreply.github.com>
2025-07-01 17:01:38 +0300

    Simplified Collapse component

--- src/components/Collapse.tsx
+++ src/components/Collapse.tsx
@@ -1,5 +1,4 @@
 import { motion } from 'framer-motion';
-import { useRef, useState, useEffect } from 'react';
 
 type Props = {
   open: boolean;
@@ -7,29 +6,15 @@ type Props = {
 };
 
 export default function Collapse({ open, children }: Props) {
-  const innerRef = useRef<HTMLDivElement>(null);
-  const [contentHeight, setContentHeight] = useState(0);
-  const [resizeSwitch, setResizeSwitch] = useState(false);
-
-  useEffect(() => {
-    const { current } = innerRef;
-    if (current) setContentHeight(current.scrollHeight);
-  }, [resizeSwitch]);
-
-  useEffect(() => {
-    const func = () => setResizeSwitch((state) => !state);
-    window.addEventListener('resize', func);
-    return () => window.removeEventListener('resize', func);
-  }, []);
-
   return (
     <motion.div
-      initial={open}
-      animate={{ height: open ? contentHeight : 0 }}
+      animate={{
+        height: open ? 'auto' : 0,
+      }}
       transition={{ duration: 0.3, ease: 'easeInOut' }}
       className="overflow-hidden"
     >
-      <div ref={innerRef}>{children}</div>
+      {children}
     </motion.div>
   );
 }
