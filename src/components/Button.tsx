b3fa9162b327ed94660242f54d5acef0a902a4b4
Oded2 <111157775+Oded2@users.noreply.github.com>
2025-07-05 12:49:04 +0300

    Refactor Button component to remove ButtonDisplay and streamline rendering logic

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -26,64 +26,44 @@ export default function Button({
   fullWidth,
   small,
 }: Props) {
+  const className = clsx(
+    'cursor-pointer border-4 px-5 font-semibold text-slate-50 shadow transition-colors disabled:pointer-events-none disabled:opacity-60',
+    {
+      'border-red-500 bg-red-500 hover:bg-red-500/90 active:bg-red-500/80':
+        style === 'primary',
+      'border-teal-600 bg-teal-600 hover:bg-teal-600/90 active:bg-teal-600/80':
+        style === 'secondary',
+      'border-gray-500 bg-gray-500 hover:bg-gray-500/90 active:bg-gray-500/80':
+        style === 'neutral',
+      'border-slate-900 bg-slate-900 hover:bg-slate-900/90 active:bg-slate-900/80':
+        style === 'black',
+      'rounded-xl': rounded,
+      'w-full': fullWidth,
+      'py-1.5': !small,
+      'py-1': small,
+    },
+  );
+
   return (
     <>
       {href ? (
         <Link
           href={href}
           target={newTab ? '_blank' : undefined}
-          className="group"
+          className={className}
         >
-          <ButtonDisplay
-            style={style}
-            rounded={rounded}
-            fullWidth={fullWidth}
-            small={small}
-            label={label}
-          />
+          {label}
         </Link>
       ) : (
         <button
           type={type}
           onClick={onClick}
           disabled={disabled}
-          className="group"
+          className={className}
         >
-          <ButtonDisplay
-            style={style}
-            rounded={rounded}
-            fullWidth={fullWidth}
-            small={small}
-            label={label}
-          />
+          {label}
         </button>
       )}
     </>
-  );
-}
-
-function ButtonDisplay({ style, rounded, fullWidth, small, label }: Props) {
-  return (
-    <div
-      className={clsx(
-        'border-4 px-5 font-semibold text-slate-50 shadow transition-colors group-enabled:cursor-pointer group-disabled:opacity-60',
-        {
-          'border-red-500 bg-red-500 group-enabled:hover:bg-red-500/90 group-enabled:active:bg-red-500/80':
-            style === 'primary',
-          'border-teal-600 bg-teal-600 group-enabled:hover:bg-teal-600/90 group-enabled:active:bg-teal-600/80':
-            style === 'secondary',
-          'border-gray-500 bg-gray-500 group-enabled:hover:bg-gray-500/90 group-enabled:active:bg-gray-500/80':
-            style === 'neutral',
-          'border-slate-900 bg-slate-900 group-enabled:hover:bg-slate-900/90 group-enabled:active:bg-slate-900/80':
-            style === 'black',
-          'rounded-xl': rounded,
-          'w-full': fullWidth,
-          'py-1.5': !small,
-          'py-1': small,
-        },
-      )}
-    >
-      {label}
-    </div>
   );
 }
