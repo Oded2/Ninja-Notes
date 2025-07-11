3f6bbebbfb828d3180678e697d2b3f65670fd0a3
Oded2 <111157775+Oded2@users.noreply.github.com>
2025-07-11 11:33:51 +0300

    Localized FormInputContainer component

--- src/components/clients/AuthClient.tsx
+++ src/components/clients/AuthClient.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import FormInput from '@/components/FormInput';
-import FormInputContainer from '@/components/FormInputContainer';
 import { defaultListName } from '@/lib/constants';
 import { authHandlers, listsCollection, usersCollection } from '@/lib/firebase';
 import {
@@ -28,6 +27,7 @@ import { addDoc, doc, getDoc, setDoc } from 'firebase/firestore';
 import Image from 'next/image';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
+import Button from '@/components/Button';
 
 export default function AuthClient() {
   const params = useSearchParams();
@@ -207,5 +207,38 @@ export default function AuthClient() {
         </div>
       </FormInputContainer>
     </div>
+  );
+}
+
+type FormInputContainerProps = {
+  title: string;
+  submitText: string;
+  handleSubmit: () => Promise<void>;
+  children: React.ReactNode;
+};
+
+function FormInputContainer({
+  title,
+  submitText,
+  handleSubmit,
+  children,
+}: FormInputContainerProps) {
+  const [inProgress, setInProgress] = useState(false);
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        setInProgress(true);
+        handleSubmit().then(() => setInProgress(false));
+      }}
+      className="flex w-full flex-col gap-4"
+    >
+      <h2 className="text-center text-xl font-semibold">{title}</h2>
+      {children}
+      <Button type="submit" style="primary" disabled={inProgress}>
+        {submitText}
+      </Button>
+    </form>
   );
 }
