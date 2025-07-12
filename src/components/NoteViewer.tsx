86f5c3fcb7e0a0aa84698e35def662f4d0c32bf2
Oded2 <111157775+Oded2@users.noreply.github.com>
2025-07-13 00:35:34 +0300

    Update className to use theme color

--- src/components/NoteViewer.tsx
+++ src/components/NoteViewer.tsx
@@ -332,7 +332,7 @@ export default function NoteViewer() {
                         async () => await deleteNote(note),
                       )
                     }
-                    className="text-rose-400"
+                    className="text-primary-light"
                   >
                     Delete
                   </button>
